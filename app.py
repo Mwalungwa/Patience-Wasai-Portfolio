e968282108c8dabcc8fb455bda76a1b059888e27
re-coder <bcislmr157921@spu.ac.ke>
2023-12-13 12:28:39 +0300

    Form data validation and copyright automatic update

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_bootstrap import Bootstrap
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from datetime import date
 import os
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
@@ -39,19 +41,45 @@ def send_email(name, email, message):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():  # put application's code here
+    current_date = date.today()
+
+    def is_valid_email(email):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        valid = re.fullmatch(regex, email)
+        if valid:
+            return True
+        else:
+            return False
+
     if request.method == 'POST':
         # Get form data
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
 
-        # Send email
-        send_email(name, email, message)
-        ref_name = name.split(" ")
-        flash(f'Hi {ref_name[0]}, your message has been succesfully sent. I will get back to you as soon an possible.')
+        #check data
+        missing_fields = []
+        if not name.strip():
+            missing_fields.append("name")
+        if not email.strip():
+            missing_fields.append("email address")
+        if not message.strip():
+            missing_fields.append("email address")
 
+        if missing_fields:
+            flash(f"Please fill in the following fields: {', '.join(missing_fields)}")
 
-    return render_template('index.html', )
+        print(is_valid_email(email))
+        if is_valid_email(email) == True:
+            # Send email
+            send_email(name, email, message)
+            first_name = name.split(" ")[0]
+            flash(
+                f'Hi {first_name}, your message has been successfully sent. I will get back to you as soon as possible.')
+        else:
+            flash("Enter a valid email")
+
+    return render_template('index.html', current_year = current_date.year)
 
 
 if __name__ == '__main__':
